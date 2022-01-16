b38c481182565cfead9d2c236ea961b32b369aaf
TimurDian <timur7dian84@gmail.com>
2022-01-16 14:13:02 +0700

    Add Client & Server

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main style={{ minHeight: "93vh" }}></main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
